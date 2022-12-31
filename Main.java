caa9bba6e5aa5106c4401fcc160ae9427ad84378
aimbot1526 <anshjaamwal2002@gmail.com>
2022-12-31 22:57:35 +0530

    Day-4

--- Main.java
+++ Main.java
@@ -1,4 +1,11 @@
+import java.util.Stack;
+
 public class Main {
     public static void main(String[] args) {
+        Stack<Character> stack = new Stack<>();
+        stack.push('a');
+        stack.push('c');
+        System.out.println(stack.pop());
+        System.out.println(stack);
     }
 }
